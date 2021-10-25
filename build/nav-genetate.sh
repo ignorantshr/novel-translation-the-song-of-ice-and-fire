5ecbc5b6c57c4f9d12eb22f7b2cd35a7ab09a70c
ignorantshr <480939289@qq.com>
2021-10-25 14:00:06 +0800

    build: 添加目录生成脚本

---
+++ build/nav-genetate.sh
@@ -0,0 +1,19 @@
+base_dir=$(pwd)"/"$0
+cd $(basename ${base_dir}/../docs)
+dirs=$(ls -d */)
+nav="\    - Home: index.md\n"
+
+for dir in ${dirs}; do
+	dir=${dir/\//}
+	nav="${nav}    - ${dir}:\n"
+	pages=$(ls ${dir})
+	for page in ${pages}; do
+		oname=${page#*/}
+		fname=${oname%.*}
+		fname=${fname/-/ }
+		nav="${nav}        - ${fname}: ${oname}\n"
+	done
+done
+nav=${nav%\\*}
+sed -i -r "/^ {4,8}-/d" ../mkdocs.yml
+sed -i "/nav/a ${nav}" ../mkdocs.yml
